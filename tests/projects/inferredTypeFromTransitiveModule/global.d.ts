6cb0021a786d13625c5f92459f4c73d2ae367d98
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-04-12 14:04:41 +0530

    updated files

---
+++ tests/projects/inferredTypeFromTransitiveModule/global.d.ts
@@ -0,0 +1,5 @@
+interface PromiseConstructor {
+  new <T>(): Promise<T>;
+}
+declare var Promise: PromiseConstructor;
+interface Promise<T> {}
---
+++ tests/projects/inferredTypeFromTransitiveModule/index.ts
@@ -0,0 +1,3 @@
+import { LazyAction, LazyModule } from './bundling';
+const lazyModule = new LazyModule(() => import('./lazyIndex'));
+export const lazyBar = new LazyAction(lazyModule, (m) => m.bar);
